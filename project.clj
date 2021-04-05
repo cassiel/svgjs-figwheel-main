a7ac0ecc11c2cf1f2d97f4baa2fc25cd48ff0256
Nick (Git client) <nick@cassiel.com>
2021-04-05 16:38:42 +0100

    Package version bump.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject threejs-figwheel-main "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.597"]
-                                  [com.bhauman/figwheel-main "0.2.3"]
+(defproject svgjs-figwheel-main "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.773"]
+                                  [com.bhauman/figwheel-main "0.2.11"]
                                   ;; optional but recommended
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]
 
