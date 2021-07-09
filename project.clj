c3a012ffc68f1cfd39a50afd1ebd426b6948d96b
Nick (Git client) <nick@cassiel.com>
2021-07-09 09:49:52 +0100

    Makefile, and staging build.

--- project.clj
+++ project.clj
@@ -1,15 +1,13 @@
 (defproject net.cassiel/svg "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.866"]
-                                  [org.clojure/core.async "1.3.618"]
-                                  [com.bhauman/figwheel-main "0.2.13"]
-                                  ;; optional but recommended
-                                  [com.bhauman/rebel-readline-cljs "0.1.4"]
-
-                                  ;; [cljsjs/svgjs "2.2.5-0"] -- TOO OLD
-                                  [com.stuartsierra/component "1.0.0"]
-                                  [net.cassiel/lifecycle "0.1.0-SNAPSHOT"]]
-                   :resource-paths ["target"]
-                   :clean-targets ^{:protect false} ["target"]}}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojurescript "1.10.866"]
+                 [org.clojure/core.async "1.3.618"]
+                 [com.stuartsierra/component "1.0.0"]
+                 [net.cassiel/lifecycle "0.1.0-SNAPSHOT"]]
+  :resource-paths ["target"]
+  :clean-targets  ^{:protect false} ["target"]
   :plugins [[com.github.liquidz/antq "RELEASE"]]
-  :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
+  :aliases {"fig"    ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:ws" ["run" "-m" "figwheel.main" "-O" "whitespace" "-bo" "dev"]}
+  :profiles {:dev {:dependencies   [[com.bhauman/figwheel-main "0.2.13"]
+                                    [com.bhauman/rebel-readline-cljs "0.1.4"]]}})
