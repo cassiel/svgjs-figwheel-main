6ab4841830b02c4fa477d5cd68618650893c616d
Nick (Git client) <nick@cassiel.com>
2021-07-09 09:40:09 +0100

    Version bump.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject net.cassiel/svg "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.844"]
-                                  [org.clojure/core.async "1.3.610"]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.866"]
+                                  [org.clojure/core.async "1.3.618"]
                                   [com.bhauman/figwheel-main "0.2.13"]
                                   ;; optional but recommended
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]
